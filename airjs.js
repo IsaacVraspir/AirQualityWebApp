17111e881611c2f1ae6af9f42c1beb082ab15352
IsaacVraspir <36165567+IsaacVraspir@users.noreply.github.com>
2018-04-03 17:05:50 -0500

    Add files via upload

--- airjs.js
+++ airjs.js
@@ -21,8 +21,34 @@ function initMap() {
 			var mylng = map.getCenter().lng();
 			document.getElementById("latitude").value = mylat;
 			document.getElementById("longitude").value = mylng;
+			
+			var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+				   // Typical action to be performed when the document is ready:
+				   console.log(xhttp.responseText);
+				   var obj = JSON.parse(xhttp.responseText);
+				   var table = document.getElementById("currentTable");
+				   
+				   if(typeof obj.results[0].city !== 'undefined'){
+						table.innerHTML = "";
+						var row = table.insertRow(0);
+						var cell = row.insertCell(0);
+						cell.innerHTML = "City";
+						cell = row.insertCell(1);
+						cell.innerHTML = "Measurement";
+						
+						row = table.insertRow(1);
+						cell = row.insertCell(0);
+						cell.innerHTML = obj.results[0].city;
+				   }
+				}
+			};
+			var params = "&coordinates=" + mylat + "," + mylng;
+			xhttp.open("GET", "https://api.openaq.org/v1/latest?"+params, true);
+			xhttp.send();
 		});
-		
+		/*
 		map.addListener('idle', function(){
 			var xhttp = new XMLHttpRequest();
 			xhttp.onreadystatechange = function() {
@@ -50,7 +76,7 @@ function initMap() {
 			xhttp.open("GET", "https://api.openaq.org/v1/latest?"+params, true);
 			xhttp.send();
 		});
-		
+		*/
 		var latlng = {lat: 44.96, lng: -93.26};
 		geocoder.geocode({'location': latlng}, function(results, status) {
 			if (status === 'OK') {
