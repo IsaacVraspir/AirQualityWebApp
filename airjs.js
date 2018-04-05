58bd26324f0756fd1a5c488e7fa274c542841f74
IsaacVraspir <36165567+IsaacVraspir@users.noreply.github.com>
2018-04-05 14:15:30 -0500

    Add files via upload

--- airjs.js
+++ airjs.js
@@ -45,9 +45,9 @@ function initMap() {
 							var cell = row.insertCell(0);
 							cell.innerHTML = obj.results[i].city;
 							cell = row.insertCell(1);
-							obj.results[i].coordinates.latitude;
+							cell.innerHTML = obj.results[i].coordinates.latitude;
 							cell = row.insertCell(2);
-							obj.results[i].coordinates.longitude;
+							cell.innerHTML = obj.results[i].coordinates.longitude;
 							cell = row.insertCell(3);
 							cell.innerHTML = obj.results[i].measurements[0].value;
 							
@@ -106,62 +106,6 @@ function initMap() {
 			xhttp.open("GET", "https://api.openaq.org/v1/latest?"+params, true);
 			xhttp.send();
 		});
-		
-		/*
-		var latlng = {lat: 44.96, lng: -93.26};
-		geocoder.geocode({'location': latlng}, function(results, status) {
-			if (status === 'OK') {
-				if (results[0]) {
-					map.setZoom(11);
-					var marker = new google.maps.Marker({
-						position: latlng,
-						map: map
-					});
-					infowindow.setContent(results[0].formatted_address);
-					infowindow.open(map, marker);
-				} else {
-					window.alert('No results found');
-				}
-			} else {
-				window.alert('Geocoder failed due to: ' + status);
-			}
-		});
-		geocodeLatLng(geocoder, map, infowindow);
-		*/
-		
-		/*
-		var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-			   // Typical action to be performed when the document is ready:
-			   console.log(xhttp.responseText);
-			}
-		};
-		/*
-		var bounds = map.getBounds();
-		var center = bounds.getCenter();
-		var ne = bounds.getNorthEast();
-
-		// r = radius of the earth in statute miles
-		var r = 3963.0;  
-
-		// Convert lat or lng from decimal degrees into radians (divide by 57.2958)
-		var lat1 = center.lat() / 57.2958; 
-		var lon1 = center.lng() / 57.2958;
-		var lat2 = ne.lat() / 57.2958;
-		var lon2 = ne.lng() / 57.2958;
-
-		// distance = circle radius from center to Northeast corner of bounds
-		var radiusMiles = r * Math.acos(Math.sin(lat1) * Math.sin(lat2) + 
-		  Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1));
-		
-		var radiusMeters = radiusMiles * 1609.34;
-		*/
-		/*
-		var params = "&coordinates=44.96,-93.26" + "&radius=" + radiusMeters;
-		xhttp.open("GET", "https://api.openaq.org/v1/latest?"+params, true);
-		xhttp.send();
-		*/
 		
 		
 	}else{ //After adding a marker
