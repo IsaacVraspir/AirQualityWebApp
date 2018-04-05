d1dfbfd2a4247c1635503f7d08563ee88f3d8581
IsaacVraspir <36165567+IsaacVraspir@users.noreply.github.com>
2018-04-05 18:38:08 -0500

    Add files via upload

--- airjs.js
+++ airjs.js
@@ -118,6 +118,7 @@ function initMap() {
 		var dateTo = new Date();
 		var n = dateTo.toISOString();
 		var dateFrom = document.getElementById("dateFrom").value;
+		console.log(dateFrom);
 		
 		var params = "&coordinates=" + latitude + "," + longitude + "&radius=" + radiusMeters + "&parameter=" + txt + "&value_from=" + valueFrom + "&value_to=" + valueTo + "&date_from=" + dateFrom + "&date_to=" + dateTo;
 		xhttp.open("GET", "https://api.openaq.org/v1/measurements?"+params, true);
